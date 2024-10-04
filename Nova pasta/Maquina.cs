6cce3ec8c7cd59d3c6de42e766d2f139c7b75a50
evellyn <evellys116@gmail.com>
2024-10-04 16:46:21 -0300

    Add files via upload

---
+++ Nova pasta/Maquina.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioInterfaces
+{
+    internal class Maquina : INaoVivo
+    {
+        public void desligar()
+        {
+        }
+
+        public void emitir_alerta()
+        {
+        }
+
+        public void ligar()
+        {
+        }
+    }
+}
